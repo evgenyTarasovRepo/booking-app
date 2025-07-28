ae0fc66931b0b92e6adb58cb95e908d86f5d86f2
Evgeny Tarasov <eatarasov@yahoo.com>
2025-07-28 16:03:43 +0300

    user-service/add user entity and add lombok

---
+++ user-service/src/main/java/com/booking/user/entity/User.java
@@ -0,0 +1,48 @@
+package com.booking.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at")
+    private LocalDateTime userCreationDate;
+
+
+    public User(String firstName, String lastName, String email) {
+        this.id = UUID.randomUUID();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.userCreationDate = LocalDateTime.now();
+    }
+
+}
