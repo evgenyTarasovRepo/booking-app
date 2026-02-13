0a53a344ac8c1b2a72ce57558ff78b530650ce68
Evgeny Tarasov <e.tarasov@x5.ru>
2026-02-13 10:56:15 +0300

    user-service/fix-review-comments: remove Mappings and add pagination and add logical removal method

--- user-service/src/main/java/com/booking/user/controller/UserController.java
+++ user-service/src/main/java/com/booking/user/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.booking.user.controller;
 
-import com.booking.user.constant.Mappings;
 import com.booking.user.dto.UserCreationDto;
 import com.booking.user.dto.UserDto;
 import com.booking.user.dto.UserPatchDto;
 import com.booking.user.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +15,7 @@ import java.util.Set;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(Mappings.BASE_USER_URL)
+@RequestMapping("/api/v1/users")
 @AllArgsConstructor
 public class UserController {
 
@@ -25,29 +26,36 @@ public class UserController {
         return ResponseEntity.ok(userService.create(userCreationDto));
     }
 
-    @GetMapping(Mappings.GET_USER_BY_ID)
+    @GetMapping("/{userId}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("userId")UUID userId) {
         return ResponseEntity.ok(userService.getById(userId));
     }
 
-    @PostMapping(Mappings.GET_USER_LIST_BY_ID)
+    @PostMapping("/batch")
     public ResponseEntity<List<UserDto>> getUserByIds(@RequestBody Set<UUID> usersIds) {
         return ResponseEntity.ok(userService.getByIds(usersIds));
     }
 
-    @GetMapping(Mappings.GET_ALL_USERS)
-    public ResponseEntity<List<UserDto>> getAll() {
-        return ResponseEntity.ok(userService.getAll());
+    @GetMapping
+    public Page<UserDto> getAll(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size) {
+        return userService.getAll(PageRequest.of(page, size));
     }
 
-    @PatchMapping(Mappings.UPDATE_OR_DELETE)
+    @PatchMapping("/{userId}")
     public ResponseEntity<UserDto> updateOrLogicalDelete(@PathVariable("userId") UUID userId,
                                                          @RequestBody UserPatchDto userPatchDto) {
         return ResponseEntity.ok(userService.update(userId, userPatchDto));
     }
 
-    @GetMapping(Mappings.GET_USER_BY_EMAIL)
-    public ResponseEntity<UserDto> getUserByEmail(@PathVariable String email) {
+    @GetMapping("{email}")
+    public ResponseEntity<UserDto> getUserByEmail(@PathVariable("email") String email) {
         return  ResponseEntity.ok(userService.getByEmail(email));
+    }
+
+    @PatchMapping("/{userId}")
+    public ResponseEntity<UserDto> changeDeleteStateForUser(@PathVariable("userId") UUID userId, @RequestParam("deleted") Boolean deleted) {
+        return ResponseEntity.ok(userService.changeDeleteStateForUser(userId, deleted));
     }
 }
