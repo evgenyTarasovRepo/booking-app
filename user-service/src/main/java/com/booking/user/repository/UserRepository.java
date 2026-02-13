183e4babf489f7f81b752745c9908da7b213946a
Evgeny Tarasov <e.tarasov@x5.ru>
2026-02-13 10:54:55 +0300

    user-service/fix-review-comments: fix naming in UserRepository

--- user-service/src/main/java/com/booking/user/repository/UserRepository.java
+++ user-service/src/main/java/com/booking/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.booking.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -11,10 +12,11 @@ import java.util.UUID;
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
 
-    Optional<User> findByIdAndDeleteFalse(UUID userId);
-    List<User> findByIdsAndDeleteFalse(Iterable<UUID> userIds);
+    Optional<User> findByIdAndIsDeletedFalse(UUID userId);
+
+    List<User> findByIdInAndIsDeletedFalse(Collection<UUID> id);
 
     Optional<User> findByEmail(String email);
 
-    List<User> findByIdIn(Iterable<UUID> userIds);
+    List<User> findByIdIn(Collection<UUID> id);
 }
