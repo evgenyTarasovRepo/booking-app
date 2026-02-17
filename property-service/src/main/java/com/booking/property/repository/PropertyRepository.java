729582f3c49e15fa16d161d3b90e3e53dde5794f
Evgeny Tarasov <e.tarasov@x5.ru>
2026-02-17 11:55:11 +0300

    property-service/crud_functional: add property BD in compose and create PropertyRepository

---
+++ property-service/src/main/java/com/booking/property/repository/PropertyRepository.java
@@ -0,0 +1,9 @@
+package com.booking.property.repository;
+
+import com.booking.property.entity.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface PropertyRepository extends JpaRepository<Property, UUID> {
+}
