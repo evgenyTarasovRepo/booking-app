22399bd6cbdcc2435238429f2a286632454707fa
Evgeny Tarasov <e.tarasov@x5.ru>
2026-02-19 19:55:11 +0300

    property-service/crud_functional: create updatePropertyEntity in mapper

--- property-service/src/main/java/com/booking/property/mapper/PropertyMapper.java
+++ property-service/src/main/java/com/booking/property/mapper/PropertyMapper.java
@@ -2,9 +2,9 @@ package com.booking.property.mapper;
 
 import com.booking.property.dto.PropertyCreationDto;
 import com.booking.property.dto.PropertyDto;
+import com.booking.property.dto.PropertyPatchDto;
 import com.booking.property.entity.Property;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.*;
 
 import java.util.List;
 
@@ -17,5 +17,14 @@ public interface PropertyMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "isActive", ignore = true)
     Property toPropertyEntity(PropertyCreationDto propertyCreationDto);
+
+    //PatchDto -> Property - update of existing Property
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "isActive", ignore = true)
+    @Mapping(target = "ownerId", ignore = true)
+    void updatePropertyEntity(PropertyPatchDto propertyPatchDto, @MappingTarget Property property);
 }
