c93a3955a9621de2c95e2aad4fc3a3580c40bb34
Evgeny Tarasov <e.tarasov@x5.ru>
2026-02-19 19:56:35 +0300

    property-service/crud_functional: add new methods in PropertyRepository and PropertyService

--- property-service/src/main/java/com/booking/property/repository/PropertyRepository.java
+++ property-service/src/main/java/com/booking/property/repository/PropertyRepository.java
@@ -2,8 +2,16 @@ package com.booking.property.repository;
 
 import com.booking.property.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface PropertyRepository extends JpaRepository<Property, UUID> {
+
+    List<Property> findAllByOwnerId(UUID userId);
+
+    List<Property> findAllByIdIn(Collection<UUID> ids);
 }
--- property-service/src/main/java/com/booking/property/service/PropertyService.java
+++ property-service/src/main/java/com/booking/property/service/PropertyService.java
@@ -6,19 +6,22 @@ import com.booking.property.dto.PropertyPatchDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
 public interface PropertyService {
     PropertyDto createProperty(PropertyCreationDto dto);
 
-    PropertyDto getPropertyById(String id);
+    PropertyDto getPropertyById(UUID id);
 
     PropertyDto updateProperty(UUID id, PropertyPatchDto dto);
 
-    Page<List<PropertyDto>> getAllProperties(Pageable pageable);
+    Page<PropertyDto> getAll(Pageable pageable);
 
     List<PropertyDto> getPropertiesByOwner(UUID id);
 
-    PropertyDto changeActivePropertyState(UUID id, Boolean active);
+    List<PropertyDto> getAllPropertiesByIds(Collection<UUID> ids);
+
+    PropertyDto changeActivePropertyState(UUID id, Boolean state);
 }
